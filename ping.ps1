e9f2916f4529855cdeaa238694cfd65e04a2f0f2
OliviaBDK <57944600+OliviaBDK@users.noreply.github.com>
2019-11-20 16:17:58 +0100

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,6 +1,5 @@
 ping 23.97.227.87 > ping.txt
-$file = "C:\ping.txt"
-$name = (Get-Item $file).Name
+$file = "ping.txt"
 $uri = "https://storagelp5tqrvnqrtuc.blob.core.windows.net/logs/$($name)sv=2015-04-05&sr=c&se=2050-01-01T00%3A00%3A00.0000000Z&sp=c&sig=7oKfr62XRi%2B2%2Bg%2FbGUt6bLw3YYM4e%2BztoeHdJdpPudg%3D"
 $headers = @{
     'x-ms-blob-type' = 'BlockBlob'
