0ec8068f4eeeb13fcf5adf427474d792b8c14a70
OliviaBDK <57944600+OliviaBDK@users.noreply.github.com>
2019-11-20 16:13:21 +0100

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,3 +1,4 @@
+ping 23.97.227.87 > ping.txt
 $file = "C:\ping.txt"
 $name = (Get-Item $file).Name
 $uri = "https://storagelp5tqrvnqrtuc.blob.core.windows.net/logs/$($name)?st=2019-04-03T07%3A28%3A36Z&se=2019-04-03T07%3A28%3A36Z&sp=rwdl&sv=2018-03-28&sr=c&sig=Y3%2BBRkH5ivySba7qAFQ%2BnjF2HoVg0Lr4bjVPrKZh6mU%3D"
