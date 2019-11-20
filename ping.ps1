a3acdffdc26ad203a67bb755255e2af8a8202ece
OliviaBDK <57944600+OliviaBDK@users.noreply.github.com>
2019-11-20 16:00:03 +0100

    Add files via upload

---
+++ ping.ps1
@@ -0,0 +1,7 @@
+$file = "C:\ping.txt"
+$name = (Get-Item $file).Name
+$uri = "https://storagelp5tqrvnqrtuc.blob.core.windows.net/logs/$($name)?st=2019-04-03T07%3A28%3A36Z&se=2019-04-03T07%3A28%3A36Z&sp=rwdl&sv=2018-03-28&sr=c&sig=Y3%2BBRkH5ivySba7qAFQ%2BnjF2HoVg0Lr4bjVPrKZh6mU%3D"
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $file 
