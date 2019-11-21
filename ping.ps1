38ac02d44faa06f1064b497b68cdc8d9017718a5
OliviaBDK <57944600+OliviaBDK@users.noreply.github.com>
2019-11-21 09:19:07 +0100

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,5 +1,5 @@
 ping 104.47.150.87 > ping.txt
-$file = "C:\ping.txt"
+$file = "C:\Users\Student\ping.txt"
 $name = (Get-Item $file).Name
 $uri = "https://storage6mnulsbypuzpu.blob.core.windows.net/logs/$($name)?sv=2015-04-05&sr=c&se=2050-01-01T00%3A00%3A00.0000000Z&sp=c&sig=T6CrwfdX7ONlKyCfUepn3b5zo0K2RJxEJz8%2BRanr3Ms%3D"
 $headers = @{
