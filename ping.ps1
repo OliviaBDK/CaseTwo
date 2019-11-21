21424887abbb68c2e1419c03879ca05e8b978a8b
OliviaBDK <57944600+OliviaBDK@users.noreply.github.com>
2019-11-21 09:17:33 +0100

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,7 +1,7 @@
-ping 23.97.227.87 > ping.txt
+ping 104.47.150.87 > ping.txt
 $file = "C:\ping.txt"
 $name = (Get-Item $file).Name
-$uri = "https://storagelp5tqrvnqrtuc.blob.core.windows.net/logs/$($name)?sv=2015-04-05&sr=c&se=2050-01-01T00%3A00%3A00.0000000Z&sp=c&sig=7oKfr62XRi%2B2%2Bg%2FbGUt6bLw3YYM4e%2BztoeHdJdpPudg%3D"
+$uri = "https://storage6mnulsbypuzpu.blob.core.windows.net/logs/$($name)?sv=2015-04-05&sr=c&se=2050-01-01T00%3A00%3A00.0000000Z&sp=c&sig=T6CrwfdX7ONlKyCfUepn3b5zo0K2RJxEJz8%2BRanr3Ms%3D"
 $headers = @{
     'x-ms-blob-type' = 'BlockBlob'
 }
